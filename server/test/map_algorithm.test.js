9d184c5638ecfa4c7b5a5c42cdcbb197aa401ce7
myyusuf <myyusuf1911@gmail.com>
2017-08-11 10:33:30 +0700

    Map algorithm

--- server/test/map_algorithm.test.js
+++ server/test/map_algorithm.test.js
@@ -1,20 +1,26 @@
 require('dotenv').config({ path: '.env.test' });
+const distance = require('geo-coords-distance');
+console.log(distance);
 
-console.log(process.env.API_KEY_1);
-
-const googleMapsClient = require('@google/maps').createClient({
-  key: process.env.API_KEY_1,
-});
+// const googleMapsClient = require('@google/maps').createClient({
+//   key: process.env.API_KEY_1,
+// });
 
 describe('Test map algorithm', () => {
   it('Should describe locations', (done) => {
-    googleMapsClient.geocode({
-      address: '1600 Amphitheatre Parkway, Mountain View, CA'
-    }, (err, response) => {
-      if (!err) {
-        console.log(JSON.stringify(response.json.results));
-      }
-      done();
-    });
+    // googleMapsClient.geocode({
+    //   address: '1600 Amphitheatre Parkway, Mountain View, CA'
+    // }, (err, response) => {
+    //   if (!err) {
+    //     console.log(JSON.stringify(response.json.results));
+    //   }
+    //   done();
+    // });
+    const firstPoint = { lat: -6.261580, lng: 106.782640 };
+    const secondPoint = { lat: -6.260855, lng: 106.781728 };
+
+    const result = distance.default(firstPoint, secondPoint);
+    done();
+    console.log(result);
   });
 });
