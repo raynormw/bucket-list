c110a5919b4d44733a6c7cdb660cf28f5979558f
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-08-14 18:54:58 +0700

    seeding progress

--- server/seeders/20170814084953-initial-seed-stores.js
+++ server/seeders/20170814084953-initial-seed-stores.js
@@ -1,17 +1,32 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Stores', [{
+      name: 'Indomaret Tanah Kusir 2',
+      lat_long: [-6.258129, 106.782308]
+    }, {
+      name: 'Ranch Market Pondok Indah',
+      lat_long: [-6.262535, 106.784161]
+    }, {
+      name: 'Alfamart Darama Putra Raya',
+      lat_long: [-6.254726, 106.783699]
+    }, {
+      name: 'Giant Blok M',
+      lat_long: [-6.243690, 106.797519]
+    }, {
+      name: 'Hero Pondok Indah Mall 1',
+      lat_long: [-6.265655, 106.784906]
+    }, {
+      name: 'Super Indo Mampang',
+      lat_long: [-6.260995, 106.829432]
+    }, {
+      name: 'Lion Parcel Fatmawati',
+      lat_long: [-6.264241, 106.798406]
+    }, {
+      name: 'Lotte Mart Fatmawati',
+      lat_long: [-6.276683, 106.797343]
+    }], {})
   },
 
   down: function (queryInterface, Sequelize) {
@@ -20,7 +35,7 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('Person', null, {});
+      return queryInterface.bulkDelete('Person', null, {})
     */
   }
-};
+}
