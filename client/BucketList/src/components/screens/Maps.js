47a71ba0056e1d3961208fde921ec0b2e8bff031
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-08-14 22:17:24 +0700

    Oke selesai routingnya

--- client/BucketList/src/components/screens/Maps.js
+++ client/BucketList/src/components/screens/Maps.js
@@ -1,7 +1,9 @@
 import React, { Components } from 'react'
 import { View, StyleSheet, Text, Dimensions } from 'react-native'
 import MapView from 'react-native-maps'
+import Polyline from '@mapbox/polyline'
 import { styleMenu, styles } from '../styles'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const { width, height } = Dimensions.get('window')
 
@@ -10,6 +12,24 @@ const SCREEN_WIDTH = width
 const ASPECT_RATIO =  width / height
 const LATITUDE_DELTA = 0.0922
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
+var dummyDataForMarkers = [{
+  title: 'Alfamart1',
+  latitude: -6.2590744,
+  longitude: 106.7817552
+}, {
+  title: 'Alfamart2',
+  latitude: -6.2592594,
+  longitude: 106.7804599
+}, {
+  title: 'Indomaret1',
+  latitude: -6.2607187,
+  longitude: 106.7772388
+}, {
+  title: 'Indomaret2',
+  latitude: -6.2505417,
+  longitude: 106.777047
+}]
 
 class Maps extends React.Component {
   constructor(props) {
@@ -24,7 +44,8 @@ class Maps extends React.Component {
       markerPosition: {
         latitude: 0,
         longitude: 0
-      }
+      },
+      coordDirections: []
     }
   }
 
@@ -61,6 +82,48 @@ class Maps extends React.Component {
       this.setState({initialPosition: lastRegion})
       this.setState({markerPosition: lastRegion})
     })
+
+    var coordGroups = []
+    var promises = []
+
+    for(let i=0; i<dummyDataForMarkers.length; i++) {
+      if(i < dummyDataForMarkers.length - 1){
+
+        const promise = new Promise((resolve, reject) => {
+          let startPos = `${dummyDataForMarkers[i].latitude},${dummyDataForMarkers[i].longitude}`
+          let destinationPos = `${dummyDataForMarkers[i+1].latitude},${dummyDataForMarkers[i+1].longitude}`
+
+          fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${ startPos }&destination=${ destinationPos }`)
+          .then((result) => {
+            let respJson = result.json()
+            .then((mypromise) => {
+              console.log('------------1', mypromise);
+              let points = Polyline.decode(mypromise.routes[0].overview_polyline.points)
+              console.log('--------------2', points);
+              let coords = points.map((point, index) => {
+                return {
+                  latitude: point[0],
+                  longitude: point[1]
+                }
+              })
+              console.log('-----------coords', coords);
+              resolve(coords);
+            })
+          })
+        })
+        promises.push(promise);
+      }
+    }
+    Promise.all(promises)
+    .then((data) => {
+      console.log('----------data', data);
+      this.setDirectionMethode(data)
+    })
+  }
+
+  setDirectionMethode(data) {
+    this.setState({coordDirections: data})
+    console.log('--------------4', this.state.coordDirections);
   }
 
   componentWillUnmount() {
@@ -68,24 +131,37 @@ class Maps extends React.Component {
   }
 
   render() {
-    console.log('initialPosition:-----------', this.state.initialPosition)
-    console.log('markerPosition:-----------', this.state.markerPosition)
+    //console.log('---------ini data',this.state.coordDirections)
     return (
       <View style={styleMenu.container}>
         <MapView
           style={styleMenu.map}
           initialRegion={this.state.initialPosition}
+          showsUserLocation={true}
+          showsCompass={true}
+          zoomEnabled={true}
         >
+          {dummyDataForMarkers.map((data, index) => (
           <MapView.Marker
+            key={index}
             coordinate={{
-              latitude: this.state.markerPosition.latitude,
-              longitude: this.state.markerPosition.longitude
+              latitude: data.latitude,
+              longitude: data.longitude
             }}
+            title={data.title}
           >
-            <View style={styleMenu.radius}>
-              <View style={styleMenu.marker}/>
-            </View>
+            <Icon name="map-marker" size={40} style={{color: '#3A539B'}} />
           </MapView.Marker>
+          ))}
+          {this.state.coordDirections.map((direction, index) => (
+              <MapView.Polyline
+                key={index}
+                coordinates={direction}
+                strokeWidth={2}
+                strokeColor="red"
+              />
+            )
+          )}
         </MapView>
       </View>
     );
