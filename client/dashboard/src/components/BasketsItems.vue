9f9e5ff3a07e2cf259b61d0fc41ed62dd208a43f
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-08-16 10:59:38 +0700

    ignoring env

--- client/dashboard/src/components/BasketsItems.vue
+++ client/dashboard/src/components/BasketsItems.vue
@@ -3,10 +3,6 @@
     <section class="section" id="form_basket_items">
       <h1 class="title"> Basket Item Form</h1>
       <div class="field">
-        <label class="label"> Basket Id </label>
-        <div class="control">
-          <input v-model='basketIdItemsForm' type="text" class="input" placeholder="Basket Id">
-        </div>
         <label class="label"> Goods Id </label>
         <div class="control">
           <input v-model='goodsIdItemsForm' type="text" class="input" placeholder="Goods Id">
@@ -25,73 +21,74 @@
         </div>
       </div>
     </section>
-    <!-- <section class="section">
+    <section class="section">
       <div class="container">
         <table class="table">
           <thead>
             <tr>
               <th> No </th>
               <th> Id </th>
-              <th> Store Name </th>
-              <th> Latitude Longitude </th>
-              <th> Delete Store </th>
-              <th> Update Store </th>
-              <th> Store Goods </th>
+              <th> Goods Id</th>
+              <th> Basket Id </th>
+              <th> Goods Barcode </th>
+              <th> Goods Name </th>
+              <th> Goods Size </th>
+              <th> Quantity </th>
+              <th> Remove Item </th>
+              <th> Update Quantity </th>
             </tr>
           </thead>
           <tfoot>
             <tr>
               <th> No </th>
               <th> Id </th>
-              <th> Store Name </th>
-              <th> Latitude Longitude </th>
-              <th> Delete Store </th>
-              <th> Update Store </th>
-              <th> Store Goods </th>
+              <th> Goods Id</th>
+              <th> Basket Id </th>
+              <th> Goods Barcode </th>
+              <th> Goods Name </th>
+              <th> Goods Size </th>
+              <th> Quantity </th>
+              <th> Remove Item </th>
+              <th> Update Quantity </th>
             </tr>
           </tfoot>
           <tbody>
-            <tr v-for='store in stores'>
-              <td> {{stores.indexOf(store) + 1}} </td>
-              <td> {{store.id}} </td>
-              <td> {{store.name}} </td>
-              <td> {{store.lat_long}} </td>
-              <td> <button class="button" type="button" @click="confirmDelete(store)" > Delete </button> </td>
-              <td> <button class="button" type="button" @click='showUpdateModal(store)'> Update </button> </td>
-              <td> <router-link :to="{ path: '/store/' + store.id}"> <button type="button" class="button"> Store Goods </button> </router-link> </td>
+            <tr v-for='basketItem in basketItems'>
+              <td> {{basketItems.indexOf(basketItem) + 1}} </td>
+              <td> {{basketItem.id}} </td>
+              <td> {{basketItem.goods_id}} </td>
+              <td> {{basketItem.basket_id}} </td>
+              <td> {{basketItem.Good.barcode}} </td>
+              <td> {{basketItem.Good.name}} </td>
+              <td> {{basketItem.Good.goods_size}} </td>
+              <td> {{basketItem.quantity}} </td>
+              <td> <button class="button" type="button" @click="confirmDelete(basketItem)" > Remove Item </button> </td>
+              <td> <button class="button" type="button" @click='showUpdateModal(basketItem)'> Update </button> </td>
             </tr>
           </tbody>
         </table>
       </div>
-    </section> -->
-    <!-- <div :class='updateModal'>
+    </section>
+    <div :class='updateModal'>
       <div class="modal-background"></div>
       <div class="modal-card">
         <header class="modal-card-head">
-          <p class="modal-card-title">Update Store</p>
+          <p class="modal-card-title">Update Quantity</p>
           <button @click='closeModal' class="delete" aria-label="close"></button>
         </header>
         <section class="modal-card-body">
           <div class="field">
-            <label class="label"> Store name </label>
+            <label class="label"> Quantity </label>
             <div class="control">
-              <input v-model='UpdateStoreName' type="text" class="input">
-            </div>
-            <label class="label"> Latitude </label>
-            <div class="control">
-              <input v-model='UpdateStoreLat' type="text" class="input">
-            </div>
-            <label class="label"> Longitude </label>
-            <div class="control">
-              <input v-model='UpdateStoreLng' type="text" class="input">
+              <input v-model='UpdateBasketItemQuantity' type="text" class="input">
             </div>
           </div>
         </section>
         <footer class="modal-card-foot">
-          <button class="button is-success" @click='submitStoreUpdate'>Save changes</button>
+          <button class="button is-success" @click='submitBasketItemUpdate'>Save changes</button>
           <button class="button" @click='closeModal'>Cancel</button>
         </footer>
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -103,15 +100,18 @@ export default {
   data () {
     return {
       basketItems: [],
-      basketIdItemsForm: '',
       goodsIdItemsForm: '',
-      quantityItemsForm: ''
+      quantityItemsForm: '',
+      updateModal: 'modal',
+      UpdateBasketItemQuantity: '',
+      UpdateBasketItemBasketId: '',
+      UpdateBasketItemGoodsId: ''
     }
   },
   methods: {
     getBasketItems: function () {
       var self = this
-      axios.get(`http://ec2-13-59-184-74.us-east-2.compute.amazonaws.com:3000/api/baskets/getitems/${self.route.params.basket_id}`)
+      axios.get(`http://ec2-13-59-184-74.us-east-2.compute.amazonaws.com:3000/api/baskets/getitems/${self.$route.params.basket_id}`)
       .then(function (basketItems) {
         self.basketItems = basketItems.data
       })
@@ -121,68 +121,67 @@ export default {
     },
     postBasketItem: function () {
       var self = this
-      // axios.post('http://ec2-13-59-184-74.us-east-2.compute.amazonaws.com:3000/api/stores', {
-      //   name: self.storeName,
-      //   lat_long: [self.storeLat, self.storeLng]
-      // })
-      // .then(function (store) {
-      //   self.getStores()
-      //   self.emptyStoreForm()
-      // })
-      // .catch(function (err) {
-      //   console.log(err)
-      // })
+      axios.post(`http://ec2-13-59-184-74.us-east-2.compute.amazonaws.com:3000//api/baskets/${self.$route.params.basket_id}/additem`, {
+        goods_id: self.goodsIdItemsForm,
+        quantity: self.quantityItemsForm
+      })
+      .then(function (store) {
+        self.getBasketItems()
+        self.emptyBasketItemForm()
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
     },
     emptyBasketItemForm: function () {
-
+      var self = this
+      self.goodsIdItemsForm = ''
+      self.quantityItemsForm = ''
+    },
+    confirmDelete: function (basketItem) {
+      var self = this
+      var choice = confirm(`Are you sure want to delete this item ?\n Id: ${basketItem.id} \n Basket Id: ${basketItem.basket_id} \n Goods Id: ${basketItem.goods_id} \n Quantity: ${basketItem.quantity} \n Goods Name: ${basketItem.Good.name}`)
+      if (choice === true) {
+        axios.delete(`http://ec2-13-59-184-74.us-east-2.compute.amazonaws.com:3000/api/baskets/${basketItem.basket_id}/${basketItem.goods_id}/removeitem`)
+        .then(function () {
+          self.getBasketItems()
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+      }
+    },
+    showUpdateModal: function (basketItem) {
+      var self = this
+      self.updateModal = 'modal is-active'
+      self.UpdateBasketItemQuantity = basketItem.quantity
+      self.UpdateBasketItemBasketId = basketItem.basket_id
+      self.UpdateBasketItemGoodsId = basketItem.goods_id
+    },
+    closeModal: function () {
+      var self = this
+      self.updateModal = 'modal'
+      self.UpdateBasketItemQuantity = ''
+      self.UpdateBasketItemBasketId = ''
+      self.UpdateBasketItemGoodsId = ''
+    },
+    submitBasketItemUpdate: function () {
+      var self = this
+      axios.put(`http://ec2-13-59-184-74.us-east-2.compute.amazonaws.com:3000//api/baskets/${self.UpdateBasketItemBasketId}/${self.UpdateBasketItemGoodsId}/editpcs`,
+        {
+          quantity: self.UpdateBasketItemQuantity
+        })
+      .then(function (result) {
+        self.closeModal()
+        self.getBasketItems()
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
     }
-    // confirmDelete: function (store) {
-    //   var self = this
-    //   var choice = confirm(`Are you sure want to delete this?\n Id: ${store.id} \n Name: ${store.name} \n Latitude Longitude: ${store.lat_long}`)
-    //   if (choice === true) {
-    //     axios.delete(`http://ec2-13-59-184-74.us-east-2.compute.amazonaws.com:3000/api/stores/${store.id}`)
-    //     .then(function (msg) {
-    //       self.getStores()
-    //     })
-    //     .catch(function (err) {
-    //       console.log(err)
-    //     })
-    //   }
-    // },
-    // showUpdateModal: function (store) {
-    //   var self = this
-    //   self.updateModal = 'modal is-active'
-    //   self.UpdateStoreId = store.id
-    //   self.UpdateStoreName = store.name
-    //   self.UpdateStoreLat = store.lat_long[0]
-    //   self.UpdateStoreLng = store.lat_long[1]
-    // },
-    // closeModal: function () {
-    //   var self = this
-    //   self.updateModal = 'modal'
-    //   self.UpdateStoreId = ''
-    //   self.UpdateStoreName = ''
-    //   self.UpdateStoreLat = ''
-    //   self.UpdateStoreLng = ''
-    // },
-    // submitStoreUpdate: function () {
-    //   var self = this
-    //   axios.put(`http://ec2-13-59-184-74.us-east-2.compute.amazonaws.com:3000/api/stores/${self.UpdateStoreId}`,
-    //     {
-    //       name: self.UpdateStoreName,
-    //       lat_long: [self.UpdateStoreLat, self.UpdateStoreLng]
-    //     })
-    //   .then(function (result) {
-    //     self.closeModal()
-    //     self.getStores()
-    //   })
-    //   .catch(function (err) {
-    //     console.log(err)
-    //   })
-    // }
   },
   created: function () {
-    this.getStores()
+    this.getBasketItems()
   }
 }
 </script>
